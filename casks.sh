37e39ea10d24b14aa82bcba3f9ffe55777d03216
Jakub Truneček <jakub.trunecek@gmail.com>
2016-05-27 10:18:43 +0200

    Adds casks

---
+++ casks.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+brew tap caskroom/fonts
+brew tap caskroom/versions
+
+# Fonts
+brew cask install \
+font-inconsolata
+
+# Apps
+brew cask install \
+phpstorm \
+webstorm \
+rubymine \
+sequel-pro \
+mysqlworkbench \
+google-chrome \
+firefox \
+iterm2 \
+spotify \
+virtualbox \
+vagrant \
+atom
